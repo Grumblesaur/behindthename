780c240b69ca06e1cde81f9294528818809fc6aa
James Murphy <james.f.murphy.one@gmail.com>
2020-12-23 14:08:56 -0500

    pip installable?

---
+++ README.md
@@ -0,0 +1,1 @@
+behindthename.py
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+  long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+  requirements = f.read().splitlines()
+
+setuptools.setup(
+  name='behindthename',
+  version='0.1.0',
+  author='James Murphy',
+  author_email='james.frank.murphy@protonmail.com',
+  description='A simple API wrapper for behindthename.com',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/Grumblesaur/behindthename',
+  install_requires=requirements,
+  packages=setuptools.find_packages(),
+  classifiers=[],
+  python_requires='>=3.6',
+)
